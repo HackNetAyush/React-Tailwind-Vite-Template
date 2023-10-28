a25de34cff5c224462e272fece93cfbbd11a9526
HackNetAyush <48079328+HackNetAyush@users.noreply.github.com>
2023-10-28 23:09:16 +0530

    Second Commit!

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app h-full w-full flex items-center justify-center'>
+      Hola!
+    </div>
+  )
+}
+
+export default App
