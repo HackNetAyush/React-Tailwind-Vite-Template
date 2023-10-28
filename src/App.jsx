39a624ca61abf5c41dc35d9eadf09fc2f5b8270d
HackNetAyush <48079328+HackNetAyush@users.noreply.github.com>
2023-10-28 21:58:12 +0000

    Third Commit!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { LogoReact } from 'react-ionicons'
+import { LogoIonic } from 'react-ionicons'
+
 import './App.css'
 
 function App() {
 
   return (
     <div className='app h-full w-full flex items-center justify-center'>
-      Hola!
+      <LogoReact
+        color={'#00000'}
+        rotate
+        height="50px"
+        width="50px"
+        onClick={() => alert('Hi, This is React!')}
+      />  +  <LogoIonic
+        color={'#00000'}
+        rotate
+        height="50px"
+        width="50px"
+        onClick={() => alert('Hi, This is Ionic!')} 
+      /> + Tailwind Template By HackNetAyush
     </div>
   )
 }
